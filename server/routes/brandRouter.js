2672789c242e9abf331af31b098e70efc43525c6
AndrewKirpanev <spacewebdeveloper@gmail.com>
2021-09-05 20:31:01 +0300

    feat(server): add a brandController

--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,7 +1,9 @@
 const Router = require('express');
 const router = new Router();
 
-router.post('/');
-router.get('/');
+const brandController = require('../controllers/brandController');
+
+router.post('/', brandController.create);
+router.get('/', brandController.getAll);
 
 module.exports = router;
